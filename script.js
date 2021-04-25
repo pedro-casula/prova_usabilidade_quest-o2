c6ce6ea10b47315ad0d51b37cdafad3b6eb0aafb
Pedro Casula <pedro.casula@hotmail.com>
2021-04-25 01:09:53 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.onclick = function(){
+    let newPokemon = input.value;
+    input.value= "";
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    list.textContent = newPokemon;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = 'Deletar Pokemon'
+
+    list.appendChild(listItem);
+
+    listBtn.onclick = function(e){
+        list.removeChild(listItem);
+
+
+    }
+    input.focus();
+}
